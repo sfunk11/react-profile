c9fc45a439793b1a5827f52afd65aee29110ecc7
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-02 09:29:38 -0400

    app initialized and component folder structure created

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <span>Created by Sam Funk 2021</span>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Hi, I'm Sam Funk!</h1>
+           
+        </div>
+    )
+}
+
+export default Header;
+
+
