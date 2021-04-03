75ca8fb6bab11ba6c256aea959b9e5203a56208b
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-02 20:01:54 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
-import Header from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
   return (
    <Wrapper>
+    <Navbar/>
     <Header/>
     <Footer/>
     </Wrapper>
