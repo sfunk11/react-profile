6d4286c07ade2fc536a9f8c568a9c81939d40bcf
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-05 19:15:18 -0400

    fixed links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,12 +6,12 @@ import Navbar from "react-bootstrap/Navbar";
 function Header() {
   return(
   <Navbar bg="light" expand="lg">
-  <Navbar.Brand className="badge-info text-white " href="#home">Samantha Funk </Navbar.Brand>
+  <Navbar.Brand className="badge-info text-white " href="/">Samantha Funk </Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link href="#About">About</Nav.Link>
-      <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
+      <Nav.Link href="/portfolio">Portfolio</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
